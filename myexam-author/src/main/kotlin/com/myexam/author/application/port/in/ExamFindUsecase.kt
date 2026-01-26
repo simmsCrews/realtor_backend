5b5d823cd135e893f7f09ae3db9159b46f033e28
futuremaker <futuremaker019@gmail.com>
2026-01-27 00:03:29 +0900

    [chore] 프로젝트 변경

--- myexam-author/src/main/kotlin/com/myexam/author/application/port/in/ExamFindUsecase.kt
+++
@@ -1,9 +0,0 @@
-package com.myexam.author.application.port.`in`
-
-import com.myexam.author.domain.Exam
-
-interface ExamFindUsecase {
-
-    fun searchExams(pageNum: Int): List<Exam>
-
-}
--- myexam-author/src/main/kotlin/com/myexam/author/application/port/out/ExamFindPort.kt
+++
@@ -1,9 +0,0 @@
-package com.myexam.author.application.port.out
-
-import com.myexam.author.domain.Exam
-
-interface ExamFindPort {
-
-    fun searchExams(pageNum: Int): List<Exam>
-
-}
