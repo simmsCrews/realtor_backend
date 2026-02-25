4e22fd4eedd7d2037be8b8f629ddee1eb97c329d
bavuchoko <bavuchoko@naver.com>
2026-02-25 17:18:24 +0900

    [update] 임대인을 vo 에서 일반 클래스로 변경

--- system-realtor/src/main/kotlin/com/system/realtor/domain/property/Landlord.kt
+++ system-realtor/src/main/kotlin/com/system/realtor/domain/property/Landlord.kt
@@ -1,10 +1,13 @@
 package com.system.realtor.domain.property
 
-data class Landlord(
+/**
+  임대인
+ * */
+class Landlord(
     val name:String,
     val mobile: String,
     val phone: String,
     val address: Address,
     val registryNumber:String,
     val description: String
-)
+){}
