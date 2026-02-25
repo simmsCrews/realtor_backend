0180f1356896c84ec90360fab06943be09a4a30a
bavuchoko <bavuchoko@naver.com>
2026-02-25 17:18:39 +0900

    [update] 매물에 임차인 소유주 필드 추가

--- system-realtor/src/main/kotlin/com/system/realtor/domain/property/Property.kt
+++ system-realtor/src/main/kotlin/com/system/realtor/domain/property/Property.kt
@@ -12,12 +12,15 @@ class Property(
    //계약 객체
    val contractEndDate: LocalDate,           // 만기일
    val price: String,                        // 가격
-   val status: String,                        // 상태
+   val status: String,                       // 상태
 
-   val landLoad: Landlord,                      // 임대(매도)인
+   // 권리당사자
+   val owner: Owner,                         // 소유주
+   val landLoad: Landlord,                   // 임대인
+   val lessee: Lessee,                       // 임차인
 
    //주소 vo
-   val address: Address,                      // 주소
+   val address: Address,                     // 주소
 
    val realtor: Realtor
 ) {
