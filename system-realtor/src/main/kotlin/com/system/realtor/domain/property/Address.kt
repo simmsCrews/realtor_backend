52dd9037eb5006c9f0b68cc71795b73519b336fa
bavuchoko <bavuchoko@naver.com>
2026-02-26 21:52:03 +0900

    [update] 주소객체에서 면적 정보 제외함

--- system-realtor/src/main/kotlin/com/system/realtor/domain/property/Address.kt
+++ system-realtor/src/main/kotlin/com/system/realtor/domain/property/Address.kt
@@ -6,7 +6,6 @@ data class Address(
     val address2: String,
     val detail: String,
     val residentialType :ResidentialType,    // 주거 형태
-    val area: String,
 //    val latitude : Long,    //위도  지도 api 연동시
 //    val longitude: Long,    //경도
 
