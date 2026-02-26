34f0a534a53cd9a6a7a58430a2dc67aa3730710a
bavuchoko <bavuchoko@naver.com>
2026-02-26 21:51:44 +0900

    [update] 계약객체에 필요한 프로퍼티 옮김

--- system-realtor/src/main/kotlin/com/system/realtor/domain/Contract.kt
+++ system-realtor/src/main/kotlin/com/system/realtor/domain/Contract.kt
@@ -1,4 +1,14 @@
 package com.system.realtor.domain
 
-class Contract {
+import com.system.realtor.domain.property.Person
+import com.system.realtor.domain.property.Property
+import java.time.LocalDate
+
+class Contract(
+    val contractEndDate: LocalDate,           // 만기일
+    val status: String,                       // 상태
+    val landLoad: Person,                     // 임대인
+    val lessee: Person,                       // 임차인
+    val property: Property                    // 매물
+) {
 }
--- system-realtor/src/main/kotlin/com/system/realtor/domain/property/Property.kt
+++ system-realtor/src/main/kotlin/com/system/realtor/domain/property/Property.kt
@@ -3,30 +3,31 @@ package com.system.realtor.domain.property
 import java.time.LocalDate
 
 import com.system.realtor.domain.Realtor
-import java.util.Properties
 
 /**
    매물
  */
 class Property(
 
-   // 계약 객체
-   val contractEndDate: LocalDate,           // 만기일
+   // 조건
    val price: Price,                         // 가격
-   val status: String,                       // 상태
+   // todo 면적 vo 추가
 
    // 희망 거래 종류
    val dealType: Set<DealType>,
 
-   // 권리당사자
-   val owner: Owner,                         // 소유주
-   val landLoad: Landlord,                   // 임대인
-   val lessee: Lessee,                       // 임차인
+   // 권리 당사자
+   val person: Person,                         // 소유주
 
-   //주소 vo
+   // 주소 vo
    val address: Address,                     // 주소
 
-   val realtor: Realtor
+   // 특이 사항 : enum
+
+
+   // 메모
+   val memo: String
+
 ) {
 
 
